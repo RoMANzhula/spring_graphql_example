8e141b703caec7e4fc8a0ce8596ea2b6988832cc
RoMANzhula <roman1985al@ukr.net>
2024-05-26 23:34:29 +0300

    Add customer repository

---
+++ src/main/java/org/romanzhula/spring_graphql_example/repositories/CustomerRepository.java
@@ -0,0 +1,14 @@
+package org.romanzhula.spring_graphql_example.repositories;
+
+import org.romanzhula.spring_graphql_example.models.Customer;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface CustomerRepository extends JpaRepository<Customer, Long> {
+    Optional<Customer> findCustomerById(Long id);
+
+    Customer findCustomerByEmail(String email);
+}
