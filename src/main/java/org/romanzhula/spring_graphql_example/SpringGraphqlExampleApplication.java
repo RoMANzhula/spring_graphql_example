9fcf92d1b09be4d298095ab02dc3986d0cfbf834
RoMANzhula <roman1985al@ukr.net>
2024-05-26 15:17:59 +0300

    Initial commit

---
+++ src/main/java/org/romanzhula/spring_graphql_example/SpringGraphqlExampleApplication.java
@@ -0,0 +1,13 @@
+package org.romanzhula.spring_graphql_example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringGraphqlExampleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringGraphqlExampleApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/romanzhula/spring_graphql_example/SpringGraphqlExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package org.romanzhula.spring_graphql_example;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringGraphqlExampleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
