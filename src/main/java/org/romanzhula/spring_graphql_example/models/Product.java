1e3c99e789bf6dd5f4aadea2533c2dff161b2e32
RoMANzhula <roman1985al@ukr.net>
2024-05-27 20:21:15 +0300

    Add product model

---
+++ src/main/java/org/romanzhula/spring_graphql_example/models/Product.java
@@ -0,0 +1,70 @@
+package org.romanzhula.spring_graphql_example.models;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+import java.math.BigDecimal;
+
+@Entity
+@Table(name = "PRODUCTS")
+public class Product {
+
+    @Id
+    @Column(name = "PRODUCT_ID")
+    private Long id;
+
+    @Column(name = "NAME")
+    private String name;
+
+    @Column(name = "SIZE")
+    private int size;
+
+    @Column(name = "PRICE")
+    private BigDecimal price;
+
+    @Column(name = "STATUS")
+    private String status;
+
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getSize() {
+        return size;
+    }
+
+    public void setSize(int size) {
+        this.size = size;
+    }
+
+    public BigDecimal getPrice() {
+        return price;
+    }
+
+    public void setPrice(BigDecimal price) {
+        this.price = price;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+}
