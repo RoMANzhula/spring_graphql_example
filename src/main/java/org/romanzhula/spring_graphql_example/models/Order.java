60a58b654a39c2f87055b7a1d3b4a554c5a1c8a5
RoMANzhula <roman1985al@ukr.net>
2024-05-27 22:40:58 +0300

    Add order model

---
+++ src/main/java/org/romanzhula/spring_graphql_example/models/Order.java
@@ -0,0 +1,57 @@
+package org.romanzhula.spring_graphql_example.models;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "ORDERS")
+public class Order {
+
+    @Id
+    @Column(name = "ORDER_ID")
+    private String id;
+
+    @ManyToOne
+    @JoinColumn(name = "CUSTOMER_ID", nullable = false, updatable = false)
+    private Customer customer;
+
+    @ManyToOne
+    @JoinColumn(name = "SELLER_ID", nullable = false, updatable = false)
+    private Seller seller;
+
+    @OneToMany(mappedBy = "order")
+    private List<OrderLine> orderLines;
+
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public Customer getCustomer() {
+        return customer;
+    }
+
+    public void setCustomer(Customer customer) {
+        this.customer = customer;
+    }
+
+    public Seller getSeller() {
+        return seller;
+    }
+
+    public void setSeller(Seller seller) {
+        this.seller = seller;
+    }
+
+    public List<OrderLine> getOrderLines() {
+        return orderLines;
+    }
+
+    public void setOrderLines(List<OrderLine> orderLines) {
+        this.orderLines = orderLines;
+    }
+
+}
